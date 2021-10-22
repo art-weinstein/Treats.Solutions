8c74c4e07b9be1616a89e5e53da90cca36890742
Art Weinstein <artur.weinstein@gmail.com>
2021-10-22 10:55:37 -0700

    create initial splash page and home controller

--- TreatShop/Controllers/HomeController.cs
+++ TreatShop/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
+using TreatShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace TreatShop.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly TreatShopContext _db;
+
+    public HomeController(TreatShopContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
-    public ActionResult Index()
-    {
-      return View();
+    public ActionResult Index() 
+    { 
+      dynamic model = new ExpandoObject();
+      model.Flavor = _db.Flavors.ToList();
+      model.Treat = _db.Treats.ToList();
+      return View(model); 
     }
   }
 }
