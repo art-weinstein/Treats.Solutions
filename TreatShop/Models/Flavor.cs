41b10e8f71b80dd6532bd9731616847cbdc6dd5c
Art Weinstein <artur.weinstein@gmail.com>
2021-10-22 09:29:40 -0700

    create initial model logic

---
+++ TreatShop/Models/Flavor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace TreatShop.Models
+{
+  public class Flavor 
+  {
+    public Flavor()
+    {
+      this.JoinEntities = new HashSet<TreatFlavor>();
+    }
+    public int FlavorId { get; set; }
+    public string FlavorName { get; set; }
+    public virtual ICollection<TreatFlavor> JoinEntities { get; }
+  }
+}
--- TreatShop/Models/Treat.cs
+++ TreatShop/Models/Treat.cs
@@ -3,4 +3,14 @@ using System.Collections.Generic;
 namespace TreatShop.Models
 {
   public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<TreatFlavor>();
+    }
+    public int TreatId { get; set; }
+    public string TreatName { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<TreatFlavor> JoinEntities { get; }
+  }
 }
