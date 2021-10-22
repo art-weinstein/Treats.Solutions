8f2307b113f8df5dd3499349640dd026488dc799
Art Weinstein <artur.weinstein@gmail.com>
2021-10-22 11:36:33 -0700

    fix controller errors, require authentication before creating a treat

--- TreatShop/Controllers/TreatController.cs
+++ TreatShop/Controllers/TreatController.cs
@@ -12,15 +12,40 @@ using System.Security.Claims;
 namespace TreatShop.Controllers
 {
   [Authorize]  
-  public class LettucePlantsController:Controller
+  public class TreatsController:Controller
   {
     private readonly TreatShopContext _db;
     private readonly UserManager <ApplicationUser> _userManager;
 
-    public LettucePlantsController(TreatShopContext db, UserManager<ApplicationUser> userManager)
+    public TreatsController(TreatShopContext db, UserManager<ApplicationUser> userManager)
     {
       _db = db;
       _userManager = userManager;
+    }
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+    public ActionResult Create()
+    {
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View();
+    }
+    [HttpPost]
+    public async Task<ActionResult> Create (Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      if(FlavorId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() {FlavorId = FlavorId, TreatId = treat.TreatId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
