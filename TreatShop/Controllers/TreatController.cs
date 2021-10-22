8748078ebd8017df1c897c513b79e727973152c9
Art Weinstein <artur.weinstein@gmail.com>
2021-10-22 14:57:18 -0700

    create functionality for editing treats and their flavors

--- TreatShop/Controllers/TreatController.cs
+++ TreatShop/Controllers/TreatController.cs
@@ -63,7 +63,7 @@ namespace TreatShop.Controllers
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
       return View(thisTreat);
     }
 
