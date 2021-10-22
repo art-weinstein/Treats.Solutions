fc55ce0f28fefe8ee8e4d2082efc957382d39634
Art Weinstein <artur.weinstein@gmail.com>
2021-10-22 15:30:50 -0700

    create functionality and view for adding a flavor to a treat

--- TreatShop/Controllers/TreatController.cs
+++ TreatShop/Controllers/TreatController.cs
@@ -81,5 +81,26 @@ namespace TreatShop.Controllers
     _db.SaveChanges();
     return RedirectToAction("Index");
     }
+
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+  [HttpPost]
+    public async Task<ActionResult> AddFlavor(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      if (FlavorId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() {FlavorId = FlavorId, TreatId = treat.TreatId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
